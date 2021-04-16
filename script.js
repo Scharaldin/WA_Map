0437528fb05bf315116fe0a61f54943d1e4acf97
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-16 10:59:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 WA.onEnterZone('myZone', () => {
 WA.sendChatMessage('Hello!', 'Pikatchu');
 
-})
+WA.openPopup("popuparea", 'test', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+});
+}]);
+
+
 
 WA.onLeaveZone('myZone', () => {
 	WA.sendChatMessage("Goodbye!", 'Pikatchu');
-})	
+	WA.onLeaveZone('myZone', () => {
+    helloWorldPopup.close();
+	}
+});	
