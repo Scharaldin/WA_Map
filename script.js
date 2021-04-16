a448296ba5ff056c4bcc1913a04360cfd37aa605
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-16 15:17:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 WA.onEnterZone('myZone', () => {
 WA.sendChatMessage('Hello!', 'Pikatchu');
-
-	WA.openPopup("popuparea", 'test', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            popup.close();
-       }
-    }];
 });
 
 
 
 WA.onLeaveZone('myZone', () => {
 	WA.sendChatMessage("Goodbye!", 'Pikatchu');
-	WA.onLeaveZone('myZone', () => {
-    helloWorldPopup.close();
-	}
+	
 });	
