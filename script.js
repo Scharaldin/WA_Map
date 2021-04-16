9a3b5f3c13258faa8077339eeba526e8a5aa17e4
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-16 11:10:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ WA.sendChatMessage('Hello!', 'Pikatchu');
         label: "Close",
         className: "primary",
         callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
             popup.close();
         }
 	}]);
