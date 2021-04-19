8cbbc0c09212f3edfc0a4cc218e1277aa00c7094
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-19 10:03:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 WA.onEnterZone('myZone', () => {
-WA.sendChatMessage('Hello!', 'Pikatchu');
+WA.sendChatMessage('Hello your Muralboard will open in about two seconds!', 'Pikatchu');
+setTimeout(2500);
 	WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a');
 });
 
 
 
 WA.onLeaveZone('myZone', () => {
-	WA.sendChatMessage("Goodbye!", 'Pikatchu');
+	WA.sendChatMessage("Goodbye have a nice day!", 'Pikatchu');
 	
 });	
