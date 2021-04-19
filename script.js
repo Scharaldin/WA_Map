7972f88330011e385f1c855a5a6b1ac91c86feaa
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-19 09:46:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 WA.onEnterZone('myZone', () => {
 WA.sendChatMessage('Hello!', 'Pikatchu');
-	WA.openTab('https://www.wikipedia.org/');
+	WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a');
 });
 
 
