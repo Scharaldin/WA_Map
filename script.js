2f4555d97996a56387b625c52e21b0697041ca8b
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-20 10:58:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ muralPopup = WA.onEnterZone('popup_zone', () =>{
 	WA.openPopup("fenster_1", 'hier gehts zu mural', [{
 		label: "Mural",
 		className: "primary",
-		callback: (fenster_1) => {
+		callback: (popup) => {
 			WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a');
-			fenster_1.close();
+			popup.close();
 			}
 		}]);
 	});
 			
 WA.onLeaveZone('popup_zone', () => {
-    fenster_1.close();
+    muralPopup.close();
 });			
