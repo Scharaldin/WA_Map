45d0d016a61745d7eab66c3fedb7f821017202c7
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-20 10:37:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 WA.onEnterZone('myZone', () => {
 WA.sendChatMessage('Hello your Muralboard will open in about two seconds!', 'Pikatchu');
-setTimeout(() => WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a') ,2500);
+setTimeout(2500);
+	WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a');
 });
 
 
@@ -9,6 +10,7 @@ WA.onLeaveZone('myZone', () => {
 	WA.sendChatMessage("Goodbye have a nice day!", 'Pikatchu');
 	
 });	
+
 
 let muralPopup;
 
