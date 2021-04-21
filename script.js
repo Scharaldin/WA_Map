240a0f5ecefd5031976dd9089c027e54f43961bb
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-21 09:46:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var zoneTest = "popup_zone";
+var muralLink = "https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a";
+var currentPopup = undefined;
+
+
 WA.onEnterZone('myZone', () => {
 WA.sendChatMessage('Hello your Muralboard will open in about two seconds!', 'Pikatchu');
 setTimeout(() => WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a'), 2500);
@@ -9,16 +14,15 @@ WA.onLeaveZone('myZone', () => {
 	WA.sendChatMessage("Goodbye have a nice day!", 'Pikatchu');
 	
 });	
+	
 
-
-let muralPopup;
-
-muralPopup = WA.onEnterZone('popup_zone', () =>{
-	WA.openPopup("test", 'Hallo ich bin eine Pflanze und ich habe ein Muralboard für dich!!!', [{
+WA.onEnterZone(zoneTest, () => {
+	curentPopup = WA.openPopup("test", 'Hallo ich bin eine Pflanze und ich habe ein Muralboard für dich!!!',[
+	{
 		label: "Zu Mural",
 		className: "primary",
 		callback: (popup) => {
-			WA.openTab('https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a');
+			WA.openTab(muralLink);
 			popup.close();
 			}
 		},{label: "Schließen",
@@ -26,10 +30,14 @@ muralPopup = WA.onEnterZone('popup_zone', () =>{
 		callback: (popup) => {
 			popup.close();
 		}
-			
-		}]);
-	});
-			
-WA.onLeaveZone('popup_zone', () => {
-    muralPopup.close();
-});			
+	}]);
+});
+
+WA.onLeaveZone(zoneTest, closePopUp)
+
+function closePopUp(){
+if(currentPopup !== undefined) {
+	currentPopup.close();
+	currentPopup = undefined;
+	}
+}
