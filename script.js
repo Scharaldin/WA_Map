5f0f482cccb0f9ea895093264a658bba9eb95141
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-27 11:17:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+//Tiled Variablen für definierte Zonen
+
 var zoneMural = "popup_mural";
-var whiteBoardZone = "popup_whiteboard";
+var zonePDF = "zone_pdf";
+var zoneYouTube = "zone_youtube";
+var zoneVideoStream = "zone_videoStream";
+var zonePictureLink = "zone_pictureLink";
+var zonePictureUpload = "zone_pictureUpload";
+var zoneAudioPlayable = "zone_audioPlayable";
+var zoneWhiteboard = "popup_whiteboard";
+
+//Variablen die auf inhalte Verweisen
+
 var muralLink = "https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a";
 var whiteboardLink = "https://www.microsoft.com/de-de/microsoft-365/microsoft-whiteboard/digital-whiteboard-app";
+var PDF = '/testPDf.pdf';
 var currentPopup = undefined;
 
 
@@ -36,7 +48,7 @@ WA.onEnterZone(zoneMural, () => {
 });
 
 
-WA.onEnterZone(whiteBoardZone, () => {
+WA.onEnterZone(zoneWhiteboard, () => {
 	closePopUp();
 	currentPopup = WA.openPopup("test_whiteboard", 'Ich bin ein Whiteboard, möchtest du mich aufrufen ?',[
 	{
@@ -54,12 +66,38 @@ WA.onEnterZone(whiteBoardZone, () => {
 	}]);
 });
 
+
+
+
+WA.onEnterZone(zonePDF, () => {
+	closePopUp();
+	currentPopup = WA.openPopup("popup_pdf", 'Ich bin ein PDF, möchtest du mich aufrufen ?',[
+	{
+		label: "Ja, bitte!",
+		className: "primary",
+		callback: (popup) => {
+			WA.openCoWebSite(PDF);
+			popup.close();
+			}
+		},{label: "Nein, lieber nicht",
+		className: "warning",
+		callback: (popup) => {
+			popup.close();
+		}
+	}]);
+});
+
 WA.onLeaveZone(zoneMural, () =>{
 	closePopUp();
 });
 
-WA.onLeaveZone(whiteBoardZone, () =>{
+WA.onLeaveZone(zoneWhiteboard, () =>{
 	closePopUp();
+});
+
+WA.onLeaveZone(zonePDF, () =>{
+	closePopUp();
+	WA.closeCoWebSite();
 });
 
 function closePopUp(){
