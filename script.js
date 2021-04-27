8f0fd1adf03a29ad6adcc2be5b592b8991b7a805
Scharaldin <43878093+Scharaldin@users.noreply.github.com>
2021-04-27 11:39:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var zoneWhiteboard = "popup_whiteboard";
 
 var muralLink = "https://app.mural.co/invitation/mural/avanade7180/1616577121346?sender=ua67182cfa5bf2c37d0600807&key=abb5a9f7-b254-47cc-a05e-f562fa87ce1a";
 var whiteboardLink = "https://www.microsoft.com/de-de/microsoft-365/microsoft-whiteboard/digital-whiteboard-app";
-var PDF = '/testPDf.pdf';
+var PDF = "https://scharaldin.github.io/WA_Map/testPDF.pdf";
 var currentPopup = undefined;
 
 
